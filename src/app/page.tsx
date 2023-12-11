88214755d6c51f0cb246b01b9f0bc27ee79c2a61
Sven Notermans <sven.notermans@live.nl>
2023-12-11 18:49:17 +0100

    added packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Mobile from '../components/mobile/Mobile';
 
 export default function Home() {
   return (
     <main className="bg-black h-screen">
-      <h2 className="text-7xl font-bold ts-bottom ">The beginning of my portfolio!</h2>
+      <Mobile />
+      {/* <h2 className="text-7xl font-bold ts-bottom ">The beginning of my portfolio!</h2> */}
 
       {/* <div className="h-56 w-56 bg-to-top"></div> */}
     </main>
---
+++ src/components/mobile/Mobile.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Link from 'next/link';
+
+
+export default function Mobile() {
+  return (
+    <div className="text-red">
+    </div>
+  );
+}
